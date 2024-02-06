4a7ae13f9d8815b8c12bb19657e98ccbfd949d92
Tobias Prima <primatjahja@gmail.com>
2024-02-06 19:36:12 +0700

    feat: handle authentication using authclient

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,8 +1,25 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from '../../../node_modules/@dfinity/auth-client/lib/cjs/index';
 
 const init = async () => { 
+
+  const authClient = await AuthClient.create();
+
+  if(authClient.isAuthenticated()){
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: ()=> {
+        handleAuthentication(authClient);
+      }
+    });
+  }
+}
+
+async function handleAuthenticated(authClient){
   ReactDOM.render(<App />, document.getElementById("root"));
 }
 
